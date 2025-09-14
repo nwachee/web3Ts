10ddd5cd43cbe7f2ec13a53add5faaf7dcc5263c
Nwachee <fuzzy245.in@gmail.com>
2025-09-14 16:25:51 +0100

    feat: integrate viem; add connect and fund actions with MetaMask detection

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-import { createWalletClient, custom } from "https://esm.sh/viem";
+import { createWalletClient, custom, createPublicClient } from "https://esm.sh/viem";
+import { contractAddress, abi } from "./constants.js";
 
 const connectButton = document.getElementById('connectButton');
 const fundButton = document.getElementById('fundButton');
+const ethAmountInput = document.getElementById('ethAmount');
 
-let walletClient 
+let walletClient
+let createPublicClient 
 
 async function connect() {
     if( typeof window.ethereum !== 'undefined' ) {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         });
+
        await walletClient.requestAddresses()
-        connectButton.innerHTML = "Connected."
+        connectButton.innerHTML = "CONNECTED!"
 
     } else {
         connectButton.innerHTML = "Please install MetaMask to use this feature."
     }
 }
 
-connectButton.onclick = connect 
+async function fund() {
+    const ethAmount = ethAmountInput.value
+    console.log(`Funding with ${ethAmount} ETH...`)
+
+    if( typeof window.ethereum !== 'undefined' ) {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+
+       const [connectedAccount] = await walletClient.requestAddresses()
+
+       publicClient = createPublicClient({
+            transport: custom(window.ethereum)
+        })
+   
+        await publicClient.simulateContract({
+            address : contractAddress,
+            abi,
+            functionName : 'fund',
+            account : connectedAccount
+        })
+    } else {
+        connectButton.innerHTML = "Please install MetaMask to use this feature."
+    }
+}
+
+    connectButton.onclick = connect 
+    fundButton.onclick = fund
